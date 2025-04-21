0f580b7564ffd3019b1324f1e4d3ed983663449d
mohameddwedar689 <mohameddwedar689@gmail.com>
2025-04-21 15:45:45 +0200

    build recipe API.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,13 +1,14 @@
 """
 Test for models
 """
+from decimal import Decimal
+from core import models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
 class ModelsTest(TestCase):
     """Test models."""
-
     def test_create_user_with_email_successful(self):
         """Test creating a user with email is successful."""
         email = 'test@example.com'
@@ -44,3 +45,18 @@ class ModelsTest(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal(5.50),
+            description='Sample recipe description.'
+        )
+        self.assertEqual(str(recipe), recipe.title)
---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+"""
+URL mappings for the recipe API.
+"""
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSets)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
